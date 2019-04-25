f856e16b072b20886cf1eab41cac40a7dc438825
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-04-24 22:48:20 -0300

    adicionando legendas para eixos e título

--- exer1.py
+++ exer1.py
@@ -1,7 +1,14 @@
 import matplotlib.pyplot as plt
 
 x=[1,2,5]
-y=[2,3,5]
+y=[2,3,7]
+
+plt.title("Meu Primeiro Gráfico com Python")
+
+#eixos
+
+plt.xlabel("Eixo X")
+plt.ylabel("Eixo Y")
 
 plt.plot(x, y)
 plt.show()
