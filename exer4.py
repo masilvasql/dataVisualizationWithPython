586b2f4374d417228573d7fda456dc2737150fde
Marcelo Abrão da Silva <masilvasql@gmail.com>
2019-04-24 23:11:21 -0300

    gráfico de disperção

---
+++ exer4.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+x =[1, 3, 5, 7, 9]
+y =[2, 3,7,10,8]
+
+
+title = 'Scatter Plot: Gráfico de dispersão'
+eixoX = 'Eixo X'
+eixoY = 'Eixo Y'
+
+plt.title(title)
+plt.xlabel(eixoX)
+plt.ylabel(eixoY)
+
+plt.scatter(x, y, label = 'Dados', color="r")
+plt.plot(x,y)
+plt.legend()
+
+plt.show()
